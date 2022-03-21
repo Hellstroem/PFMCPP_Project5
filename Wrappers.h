fb42420ba7e047dd0a25f09b18cf4dfa59eba27f
hellstroem11 <42948113+Hellstroem@users.noreply.github.com>
2022-03-21 19:38:47 +0000

    Fixed requested changes

--- Wrappers.h
+++ Wrappers.h
@@ -1,64 +1,85 @@
 #pragma once
 
-#include "CoffeeMachine.cpp"
-#include "Fridge.cpp"
-#include "Amplifier.cpp"
-#include "Kitchen.cpp"
-#include "House.cpp"
+#include "CoffeeMachine.h"
+#include "Fridge.h"
+#include "Amplifier.h"
+#include "Kitchen.h"
+#include "House.h"
 
 struct CoffeeMachineWrapper
 {
-    CoffeeMachineWrapper(CoffeeMachine* ptr) : coffPtr(ptr) {}
-    ~CoffeeMachineWrapper()
-    {
-        delete coffPtr;
-    }
+    CoffeeMachineWrapper(CoffeeMachine* ptr);
+    ~CoffeeMachineWrapper();
 
     CoffeeMachine* coffPtr = nullptr;
 
 };
 
+CoffeeMachineWrapper::CoffeeMachineWrapper(CoffeeMachine* ptr) : coffPtr(ptr) {}
+
+CoffeeMachineWrapper::~CoffeeMachineWrapper()
+{
+    delete coffPtr;
+}
+
+
 struct FridgeWrapper
 {
-    FridgeWrapper(Fridge* ptr) : fridgePtr(ptr) {}
-    ~FridgeWrapper()
-    {
-        delete fridgePtr;
-    }
+    FridgeWrapper(Fridge* ptr);
+    ~FridgeWrapper();
 
     Fridge* fridgePtr = nullptr;
 };
 
+FridgeWrapper::FridgeWrapper(Fridge* ptr) : fridgePtr(ptr) {}
+
+FridgeWrapper::~FridgeWrapper()
+{
+    delete fridgePtr;
+}
+
 struct AmpWrapper
 {
-    AmpWrapper(Amplifier* ptr) : ampPtr(ptr) {}
-    ~AmpWrapper()
-    {
-        delete ampPtr;
-    }
+    AmpWrapper(Amplifier* ptr);
+    ~AmpWrapper();
 
     Amplifier* ampPtr = nullptr;
 
 };
 
+AmpWrapper::AmpWrapper(Amplifier* ptr) : ampPtr(ptr) {}
+
+AmpWrapper::~AmpWrapper()
+{
+    delete ampPtr;
+}
+
 struct KitchenWrapper
 {
-    KitchenWrapper(Kitchen* ptr) : kitPtr(ptr) {}
-    ~KitchenWrapper()
-    {
-        delete kitPtr;
-    }
+    KitchenWrapper(Kitchen* ptr);
+    ~KitchenWrapper();
 
     Kitchen* kitPtr = nullptr;
 };
 
+KitchenWrapper::KitchenWrapper(Kitchen* ptr) : kitPtr(ptr) {}
+
+KitchenWrapper::~KitchenWrapper()
+{
+    delete kitPtr;
+}
+
 struct HouseWrapper
 {
-    HouseWrapper(House* ptr) : housePtr(ptr) {}
-    ~HouseWrapper()
-    {
-        delete housePtr;
-    }
+    HouseWrapper(House* ptr);
+    ~HouseWrapper();
 
     House* housePtr = nullptr;
 };
+
+HouseWrapper::HouseWrapper(House* ptr) : housePtr(ptr) {}
+
+HouseWrapper::~HouseWrapper()
+{
+    delete housePtr;
+}
